e4e734d92c37b0fb0393e91b08af84c8f54dbca9
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-01-10 11:34:19 +0800

    feat: managed to display about, contact me and 404 page

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = http.createServer(async (request, response) => {
-  if (request.method === "GET") {
-    let filePath;
-    if (request.url === "/") {
-      filePath = path.join(__dirname, "public", "index.html");
-    }
+  try {
+    if (request.method === "GET") {
+      let filePath;
+      if (request.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (request.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else if (request.url === "/contact-me") {
+        filePath = path.join(__dirname, "public", "contact-me.html");
+      } else {
+        filePath = path.join(__dirname, "public", "404.html");
+      }
 
-    const data = await fs.readFile(filePath);
-    response.setHeader("Content-Type", "text/html");
-    response.write(data);
-    response.end();
-  } else {
-    throw new Error("Method request not allowed");
+      const data = await fs.readFile(filePath);
+      response.setHeader("Content-Type", "text/html");
+      response.write(data);
+      response.end();
+    } else {
+      throw new Error("Method request not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
   }
 });
 
